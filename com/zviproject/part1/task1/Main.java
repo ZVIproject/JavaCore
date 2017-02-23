51f3619b6e4a2c3d845c406bb900c61b03cb1760
Zarovnyy Vladuslav <zarovni03@gmail.com>
2017-02-23 22:16:03 +0200

    Contined working with array #1

--- com/zviproject/part1/task1/Main.java
+++ com/zviproject/part1/task1/Main.java
@@ -9,35 +9,59 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		Scanner inputData = new Scanner(System.in);
 		Part2 tasks = new Part2();
-		//
-		// tasks.commonMyltiple();
-		// tasks.resultOfRandom();
-		// tasks.searchX();
-		// tasks.workingWithArray();
-		ArrayList<Report> reports = new ArrayList<>();
+		System.out.println("Change part \n" + "Part 1 - 1\n" + "Part 2 - 2\n" + "Part 3 - 3\n" + "Part 4 - 4");
+		int part = inputData.nextInt();
 
-		System.out.print("Number of students : ");
-		int numberOfStudents = inputData.nextInt();
-
-		for (int i = 0; i < numberOfStudents; i++) {
-
-			System.out.println("\nName : ");
-			String name = inputData.next();
-
-			System.out.print("\nGroup : ");
-			int numberOfGroup = inputData.nextInt();
-
-			System.out.print("\nReport book : ");
-			int reportBook = inputData.nextInt();
-			Report report = new Report();
-			Report rep = new Report(numberOfGroup, reportBook, name, report.addMarks());
-			reports.add(rep);
+		switch (part) {
+		case 1: {
+			tasks.commonMyltiple();
+			tasks.resultOfRandom();
+			tasks.searchX();
+			tasks.workingWithArray();
+			break;
 		}
 
-		for (Report show : reports) {
-			System.out.println(show.getSurnameOfStudent());
+		case 2: {
+			ArrayList<Report> reports = new ArrayList<>();
+
+			System.out.print("Number of students : ");
+			int numberOfStudents = inputData.nextInt();
+
+			for (int i = 0; i < numberOfStudents; i++) {
+
+				System.out.println("\nName : ");
+				String name = inputData.next();
+
+				System.out.print("\nGroup : ");
+				int numberOfGroup = inputData.nextInt();
+
+				System.out.print("\nReport book : ");
+				int reportBook = inputData.nextInt();
+
+				Report rep = new Report(numberOfGroup, reportBook, name, addMarks());
+				reports.add(rep);
+			}
+
+			for (Report show : reports) {
+				System.out.println(show.getSurnameOfStudent());
+			}
+
+			break;
+		}
 		}
 
+	}
+
+	public static int[] addMarks() {
+		Scanner scan = new Scanner(System.in);
+		int arr[] = new int[4];
+
+		for (int i = 0; i < 4; i++) {
+			System.out.println("Input mark");
+			arr[i] = scan.nextInt();
+		}
+		scan.close();
+		return arr;
 	}
 
 }
--- com/zviproject/part1/task1/Report.java
+++ com/zviproject/part1/task1/Report.java
@@ -1,7 +1,5 @@
 package com.zviproject.part1.task1;
 
-import java.util.Scanner;
-
 public class Report {
 	private int numberOfGroup;
 	private int numberOfReportBook;
@@ -48,18 +46,6 @@ public class Report {
 
 	public void setMaks(int[] maks) {
 		this.maks = maks;
-	}
-
-	public int[] addMarks() {
-		Scanner scan = new Scanner(System.in);
-		int arr[] = new int[4];
-
-		for (int i = 0; i < 4; i++) {
-			System.out.println("Input mark");
-			scan.nextInt();
-		}
-		scan.close();
-		return arr;dsd
 	}
 
 }
